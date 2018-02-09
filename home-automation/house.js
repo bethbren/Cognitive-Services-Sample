6c70533bd471cf743c12e2b2510bbe217415729f
bethbren <bethfbrennan@gmail.com>
2018-02-09 17:41:25 -0500

    manipulate house with luis results

--- home-automation/house.js
+++ home-automation/house.js
@@ -3,7 +3,7 @@ const house = (() => {
     const room = {
         bedroom: "bedroom",
         kitchen: "kitchen",
-        livingRoom: "living room",
+        livingRoom: "livingroom",
         office: "office"
     };
 
@@ -26,20 +26,37 @@ const house = (() => {
         [room.office]: [device.lights, device.computer]
     };
 
-    function command(house, room, device, state) {
-        if (!devicesInRooms[room]) {
-            alert(`[Error] The house does not have a room called the "${room}"`);
+    function command(house, selectedRoom, selectedDevice, selectedState) {
+        if (!devicesInRooms[selectedRoom]) {
+            alert(`[Error] The house does not have a room called the "${selectedRoom}"`);
             return;
         }
 
-        const deviceInRoom = devicesInRooms[room].includes(device);
+        const deviceInRoom = devicesInRooms[selectedRoom].includes(selectedDevice);
 
         if (deviceInRoom) {
             // TODO pass the house DOM object as the `house` parameter
             // TODO set device state if it is in the room
-            alert(`Got it: turn ${state} the ${device} in the ${room}`);
+            const selector = "." + selectedRoom + "." + selectedDevice;
+            if(selectedState == device.state.on) {
+                if (selectedDevice == device.lights) {
+                    house.find(selector).attr("fill", "yellow");
+                }
+                else if (selectedDevice == device.tv || selectedDevice == device.computer) {
+                    house.find(selector).attr("fill", "blue");
+                }
+                else {
+                    house.find(selector).attr("stroke", "red");
+                }
+            } else {
+                if(selectedDevice == device.radio || selectedDevice == device.oven) {
+                    house.find(selector).attr("stroke", "black");
+                }
+                house.find(selector).attr("fill", "white");
+            }
+            alert(`Got it: turn ${selectedState} the ${selectedDevice} in the ${selectedRoom}`);
         } else {
-            alert(`[Error] The ${room} does not have a ${device}!`)
+            alert(`[Error] The ${selectedRoom} does not have a ${selectedDevice}!`)
         }
     }
 
