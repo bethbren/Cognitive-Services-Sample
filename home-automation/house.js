24d06c15d505d8011a25ef60707db3b9c4a528d9
Brian Cristante <none>
2018-02-10 00:22:22 -0500

    Match LUIS entities with lists of synonyms

--- home-automation/house.js
+++ home-automation/house.js
@@ -3,7 +3,7 @@ const house = (() => {
     const room = {
         bedroom: "bedroom",
         kitchen: "kitchen",
-        livingRoom: "livingroom",
+        livingRoom: "livingroom", // SVG selectors can't have spaces
         office: "office"
     };
 
@@ -27,25 +27,50 @@ const house = (() => {
     };
 
     function command(house, selectedRoom, selectedDevice, selectedState) {
-        if (!devicesInRooms[selectedRoom]) {
+        // Normalize the entities coming from LUIS by matching them with "synonyms"
+        const matchedRoom = (() => {
+            switch (selectedRoom) {
+                case "bed room":
+                    return room.bedroom;
+                case "living room":
+                    return room.livingRoom;
+                default:
+                    return selectedRoom;
+            }
+        })();
+
+        const matchedDevice = (() => {
+            switch (selectedDevice) {
+                case "light":
+                    return device.lights;
+                case "stereo":
+                    return device.radio;
+                case "television":
+                    return device.tv;
+                default:
+                    return selectedDevice;
+            }
+        })();
+
+        if (!devicesInRooms[matchedRoom]) {
             alert(`[Error] The house does not have a room called the "${selectedRoom}"`);
             return;
         }
 
-        const deviceInRoom = devicesInRooms[selectedRoom].includes(selectedDevice);
+        const deviceInRoom = devicesInRooms[matchedRoom].includes(matchedDevice);
 
         if (deviceInRoom) {
-            const selector = "." + selectedRoom + "." + selectedDevice;
+            const selector = "." + matchedRoom + "." + matchedDevice;
             if (selectedState === device.state.on) {
-                if (selectedDevice === device.lights) {
+                if (matchedDevice === device.lights) {
                     house.find(selector).attr("fill", "yellow");
-                } else if (selectedDevice === device.tv || selectedDevice === device.computer) {
+                } else if (matchedDevice === device.tv || matchedDevice === device.computer) {
                     house.find(selector).attr("fill", "blue");
                 } else {
                     house.find(selector).attr("stroke", "red");
                 }
             } else {
-                if (selectedDevice === device.radio || selectedDevice === device.oven) {
+                if (matchedDevice === device.radio || matchedDevice === device.oven) {
                     house.find(selector).attr("stroke", "black");
                 }
                 house.find(selector).attr("fill", "white");
